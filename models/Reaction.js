ee7725051a9159425a3b98947c8b723a8eee1383
Joseph Murray <josephl.murrayjr@gmail.com>
2022-05-21 18:03:45 -0400

    initial commit with boilerplate taken from NoSQL mini project

---
+++ models/Reaction.js
@@ -0,0 +1,37 @@
+const { Schema, Types } = require("mongoose");
+
+const formatDate = (date) => {
+  return date.toLocaleString();
+};
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      maxlength: 280,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: formatDate,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Reaction = model("reaction", reactionSchema);
+
+module.exports = Reaction;
---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+
+const validateEmail = (email) => {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+};
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: { validateEmail, message: "Please enter a valid email" },
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJson: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
