225d29aa894d1340c58039c8ee4e532d6cc32994
Joseph Murray <josephl.murrayjr@gmail.com>
2022-05-21 23:48:32 -0400

    created api routes for users and thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -90,4 +90,20 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  // Add a thought to a user
+  addThought(req, res) {
+    console.log("You are adding a Thought");
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { thoughts: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+
+const {
+  getThoughts,
+  getSingleThought,
+  deleteThought,
+  updateThought,
+  addReaction,
+  removeReaction,
+  addThought,
+} = require("../../controllers/thoughtController");
+
+// /api/thoughts
+router.route("/").get(getThoughts).post(addThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reaction
+router.route("/:thoughtId/reaction").put(addReaction);
+
+// /api/thoughts/:thoughtId/:reactionId
+router.route("/:thoughtId/:reactionId").delete(removeReaction);
+
+module.exports = router;
