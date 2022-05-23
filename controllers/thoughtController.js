dadd2e2303cb11ccaa814aab8c90d07b91eb0588
Joseph Murray <josephl.murrayjr@gmail.com>
2022-05-23 08:00:56 -0400

    added populate to appropriate routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,6 +4,7 @@ module.exports = {
   //get a thought
   getThoughts(req, res) {
     Thought.find()
+      .populate("reactions")
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
